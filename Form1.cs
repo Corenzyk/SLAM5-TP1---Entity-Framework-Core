a11c6c3b54d46586115e110620d77006d85cf078
Corenzyk K. <corentin.kuczyk49@gmail.com>
2024-09-05 10:04:51 +0200

    Ajoutez des fichiers projet.

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace SLAM5_TP1___Entity_Framework_Core
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
